376190d9378e8184525d17b0d407f26623d6ae41
chandana03 <chandanareshma01@gmail.com>
2022-03-05 01:22:12 +0530

    employeewage

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace 
+{
+    /// <summary>
+    /// contains main method
+    /// </summary>
+    class program
+    {
+        /// <summary>
+        /// Displaying Greeting message
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage program");
+            //employeecheck.Employeeattendence();
+
+            //empwage.Employeewages();
+
+            //empswitch.Employeeswitchcase();
+
+            //maxworkingday.workingday();
+            //calculatewages.calculate_wage();
+
+            employeecheck obj = new employeecheck();
+
+            //obj.calculate_wage();
+            obj.Employeeattendence();
+
+
+            
+        }
+}
+
+}
