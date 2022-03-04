a3a6dd32ed3ba0aaed66b2716d5bf8b98f8b5de1
chandana03 <chandanareshma01@gmail.com>
2022-03-05 01:23:08 +0530

    employee check

---
+++ employeecheck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeewages_Day3
+{
+    public class employeecheck
+    {
+        public  void Employeeattendence()
+        {
+            int Full_Time = 1;
+
+            Random random = new Random();
+            int empcheck = random.Next(2);
+
+            if (empcheck == Full_Time)
+            {
+                Console.WriteLine("employee is Present");
+
+            }
+            else
+                Console.WriteLine("Employee is absent");
+        }
+    }
+}
